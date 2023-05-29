8c1eecb51eeac9c3e15a3a183e7d070fb8c4076a
jiaxuewen <jiaxuewen2006@163.com>
2023-05-29 15:55:54 +0800

    fix(example): 给example添加eslint

---
+++ packages/example/.eslintrc.js
@@ -0,0 +1,25 @@
+/* eslint-disable */
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/recommended"
+    ],
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module",
+        "ecmaFeatures": {
+          "modules": true
+        }
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+    }
+}
